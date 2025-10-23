91ac29a35478aaa41ac76b00077bce30a20f1407
SanchitJain28 <mythichuman28@gmail.com>
2025-10-23 23:25:00 +0530

    changes

---
+++ public/fuckme.js
@@ -0,0 +1,6 @@
+for(let i=0; i<100;i++){
+ console.log("THIS IS JUST A RANDOM ASS FILE")
+}
+function SayHi(string){
+ console.log(string + "chutiye , tere jese hoshihyar chutiya mene kahi na dekha")
+}
